285cca448a21097e25a7191761917036241ef4b4
岛田悠米 <115274737@qq.com>
2024-06-21 00:35:45 +0800

    [ADD] 提交第3节：策略概率装配处理

---
+++ big-market-domain/src/main/java/cn/bugstack/domain/strategy/repository/IStrategyRepository.java
@@ -0,0 +1,23 @@
+package cn.bugstack.domain.strategy.repository;
+
+import cn.bugstack.domain.strategy.model.entity.StrategyAwardEntity;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Fuzhengwei bugstack.cn @小傅哥
+ * @description 策略服务仓储接口
+ * @create 2023-12-23 09:33
+ */
+public interface IStrategyRepository {
+
+    List<StrategyAwardEntity> queryStrategyAwardList(Long strategyId);
+
+    void storeStrategyAwardSearchRateTable(Long strategyId, Integer rateRange, Map<Integer, Integer> strategyAwardSearchRateTable);
+
+    Integer getStrategyAwardAssemble(Long strategyId, Integer rateKey);
+
+    int getRateRange(Long strategyId);
+
+}
